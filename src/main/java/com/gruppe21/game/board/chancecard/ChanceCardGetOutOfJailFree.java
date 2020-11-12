9d4002530c7832440385027e267d105dd1371110
s205436 <s205436@student.dtu.dk>
2020-11-12 15:52:02 +0100

    Added comment in ChanceCardGetOutOfJailFree regarding Game should check for if Player has a GetOutOfJailCard

--- src/main/java/com/gruppe21/game/board/chancecard/ChanceCardGetOutOfJailFree.java
+++ src/main/java/com/gruppe21/game/board/chancecard/ChanceCardGetOutOfJailFree.java
@@ -1,14 +1,22 @@
 package com.gruppe21.game.board.chancecard;
 
+import com.gruppe21.game.Game;
+
 public class ChanceCardGetOutOfJailFree extends ChanceCard{
 
     public ChanceCardGetOutOfJailFree(String description){
         super(description);
     }
 
-    public void use(){
-
+    /**
+     * Game should check for if Player has
+     *  a GetOutOfJailCard.
+     *
+     *  To-do Player should have a list of
+     *  owned ChanceCards.
+    */
+    public void use(Game game){
+        game.getGuiWrapper().showMessage(description);
     }
-
 
 }
